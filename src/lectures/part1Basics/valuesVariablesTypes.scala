de0ad5ac3e7de88c45021a75924431456002ba46
sagar <sagar.wgl17@gmail.com>
2021-02-21 15:19:47 +0530

    update code

--- src/lectures/part1Basics/valuesVariablesTypes.scala
+++ src/lectures/part1Basics/valuesVariablesTypes.scala
@@ -2,5 +2,18 @@ package lectures.part1Basics
 
 object valuesVariablesTypes extends App{
 
+  val x: Int = 425454
+  val al : Long = 4521541541541L
+  val adouble: Double = 3.14
+  val afloat:Float = 3.0f
+  println(f"$x")
+
+  val astring: String = "Hello sagar"
+
+  val achar:Char = 'a'
+  val bool:Boolean = true
+
+
+
 
 }
